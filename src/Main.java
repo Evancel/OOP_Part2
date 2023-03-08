642429773b68c5307d85df6d192269e91ead1cd2
Evancel <w.lexandra@gmail.com>
2023-03-08 19:05:23 +0300

    the first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car [] cars = {new Car(),new Car()};
+        cars[0].setModelName("car1");
+        cars[1].setModelName("car2");
+        cars[0].setWheelsCount(4);
+        cars[1].setWheelsCount(4);
+
+        Truck [] trucks = {new Truck(),new Truck()};
+        trucks[0].setModelName("truck1");
+        trucks[1].setModelName("truck2");
+        trucks[0].setWheelsCount(6);
+        trucks[1].setWheelsCount(8);
+
+        Bicycle[] bicycles = {new Bicycle(),new Bicycle()};
+        bicycles[0].setModelName("bicycle1");
+        bicycles[1].setModelName("bicycle2");
+        bicycles[0].setWheelsCount(2);
+        bicycles[1].setWheelsCount(2);
+
+        ServiceStation serviceStation = new ServiceStation();
+
+        serviceStation.check(cars);
+        serviceStation.check(trucks);
+        serviceStation.check(bicycles);
     }
 }
---
+++ src/Service.java
@@ -0,0 +1,5 @@
+public interface Service {
+    public void check(Car[] cars);
+    public void check(Truck[] trucks);
+    public void check(Bicycle[] bicycles);
+}
